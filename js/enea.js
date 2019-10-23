90a35f313b558f768ce3c66779b6cff879872830
Carla <carligir@gmail.com>
2019-10-23 13:36:07 -0300

    Agregue menu desplegable sin animacion

---
+++ js/enea.js
@@ -0,0 +1,6 @@
+var menu = document.getElementById('menu');
+var desplegable = document.getElementById('desplegable');
+menu.addEventListener('click', ()=>{
+    // Acá quiero hacer el menú desplegable
+    desplegable.classList.toggle('hidden');
+})
